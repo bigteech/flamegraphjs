56deb98c714489546ab1b8f241a670b5d5a229e0
weipeng.wang <weipeng.wang@gllue.com>
2020-05-05 14:21:27 +0800

    init

--- babel-transform.js
+++ babel-transform.js
@@ -8,6 +8,11 @@ const tmp = parser.parse(`window.flamegraphjs.collect("path")("body")`, {
     sourceType: "script",
 }).program.body[0].expression;
 
+const tmp2 = parser.parse(`var name = window.flamegraphjs.collect("path")("body")`, {
+    sourceType: "script",
+}).program.body[0];
+
+
 const getCollectAst = (t, path, body) => {
     const ret = {
         ...tmp
@@ -19,7 +24,49 @@ const getCollectAst = (t, path, body) => {
     ret.callee.arguments = [t.stringLiteral(path)];
     return t.expressionStatement(ret);
 };
-let count = 0;
+const getCollectAst2 = (t, path, body) => {
+    const name = body.id.name;
+    const ret = {
+        ...tmp2
+    };
+    ret.declarations = [...ret.declarations];
+    ret.declarations[0] = {
+        ...ret.declarations[0]
+    };
+    ret.declarations[0].id.name = name;
+
+    ret.declarations[0].init = {
+        ...ret.declarations[0].init
+    };
+    const caller = ret.declarations[0].init;
+
+    caller.callee = {
+        ...caller.callee
+    };
+    caller.arguments = [body];
+    caller.callee.arguments = [t.stringLiteral(path)];
+    return t.variableDeclaration('var', ret.declarations);
+};
+
+const trans = (path, state, api) => {
+    if (path.node.done) {
+        return;
+    }
+    path.node.done = 1;
+    path.replaceWith(getCollectAst(api.types, state.file.opts.filename, path.node));
+
+};
+
+const trans2 = (path, state, api) => {
+    if (path.node.done) {
+        return;
+    }
+    path.node.done = 1;
+    path.replaceWith(getCollectAst2(api.types, state.file.opts.filename, path.node));
+
+};
+
+
 exports.default = helperPluginUtils.declare((api, options) => {
     const {
         spec
@@ -27,13 +74,17 @@ exports.default = helperPluginUtils.declare((api, options) => {
     return {
         name: "wrapper-functions",
         visitor: {
+            FunctionDeclaration(path, state) {
+                if (!path.isFunctionDeclaration()) return;
+                trans2(path, state, api);
+            },
+            FunctionExpression(path, state) {
+                if (!path.isFunctionExpression()) return;
+                trans(path, state, api);
+            },
             ArrowFunctionExpression(path, state) {
                 if (!path.isArrowFunctionExpression()) return;
-                if (path.node.done) {
-                    return;
-                }
-                path.node.done = 1;
-                path.replaceWith(getCollectAst(api.types, state.file.opts.filename, path.node));
+                trans(path, state, api);
             }
         }
     };
--- example.js
+++ example.js
@@ -1,19 +1,3 @@
-const normalOutput = window.flamegraphjs.normalOutput;
-
-const main2 = function(x) {
-    return x + 1;
-};
-
-const main1 = () => {
-    let ret = 0;
-    for (let i = 0; i < 2; i++) {
-        ret += main2(i);
-    }
-    return ret;
-};
-const main = () => {
-    main1();
-    const input = document.getElementById('main');
-    input.value = normalOutput();
-};
-window.main = main;
+function main3(){
+    console.log('22l');
+}
